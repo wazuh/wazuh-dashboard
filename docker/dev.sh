d0e564109fde176590890655e08b26bf6c2c5f8a
yenienserrano <ian.serrano@wazuh.com>
2023-02-27 11:53:17 +0100

    Add docker files

---
+++ docker/dev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+COMPOSE_FILE=dev.yml
+COMPOSE_CMD="docker compose -f $COMPOSE_FILE"
+
+export REPO_PATH="../"
+export SECURITY_PLUGIN_REPO_PATH="../../wazuh-security-dashboards-plugin"
+export WZD_CONFIG_PATH="./config/wzd/opensearch_dashboards.yml"
+export NODE_VERSION=$(cat $REPO_PATH/.nvmrc)
+export OPENSEARCH_VERSION=$1 || $(node -e "console.log(require('$REPO_PATH/package.json').version)")
+
+case $2 in
+    up)
+        $COMPOSE_CMD up -d
+        ;;
+    down)
+        $COMPOSE_CMD down
+        ;;
+    stop)
+        $COMPOSE_CMD stop
+        ;;
+    *)
+        echo "Usage: $0 {up|down|stop}"
+        exit 1
+esac
