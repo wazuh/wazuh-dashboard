bcb1a4fd4e2c5d0428bab98705a1617ed731cd7e
Nicolas Guevara <nicolas.guevara@wazuh.com>
2023-06-27 12:45:07 -0300

    Optimize deb package generation

--- dev-tools/build-packages/deb/build_package.sh
+++ dev-tools/build-packages/deb/build_package.sh
@@ -9,19 +9,14 @@
 # Foundation.
 
 current_path="$( cd $(dirname $0) ; pwd -P )"
-architecture="amd64"
 outdir="${current_path}/output"
 revision="1"
+architecture="amd64"
 build_docker="yes"
 deb_amd64_builder="deb_dashboard_builder_amd64"
 deb_builder_dockerfile="${current_path}/docker"
-base_cmd=""
-url=""
 build_base="yes"
-security_url=""
-dashboard_path=""
-dashboard_url=""
-security_path=""
+package=""
 version=""
 
 trap ctrl_c INT
@@ -30,7 +25,7 @@ clean() {
     exit_code=$1
 
     # Clean the files
-    rm -rf ${dockerfile_path}/{*.sh,*.tar.gz,wazuh-*,*.tar.gz,*.zip}
+    rm -rf ${dockerfile_path}/{*.sh,*.tar.gz,wazuh-*,*.tar.gz,*.zip} wazuh-dashboard.tar.gz wazuh-dashboard-base
 
     exit ${exit_code}
 }
@@ -40,88 +35,55 @@ ctrl_c() {
 }
 
 build_deb() {
-    container_name="$1"
-    dockerfile_path="$2"
+  container_name="$1"
+  dockerfile_path="$2"
 
+  valid_url='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
 
-        # Verify that the necessary fields are provided
-    if [ ! "${dashboard_path}" ] && [ ! "${dashboard_url}" ];then
-        echo "No dashboard url or path provided"
-        clean 1
+  if [[ $package =~ $valid_url ]];then
+    if ! curl --output wazuh-dashboard.tar.gz --silent --fail "${package}"; then
+      echo "The given URL or Path to the Wazuh Dashboard package is not working: ${package}"
+      clean 1
     fi
+  else
+    echo "The given URL or Path to the Wazuh Dashboard package is not valid: ${package}"
+    clean 1
+  fi
 
-    if [ ! "${security_path}" ] && [ ! "${security_url}" ];then
-        echo "No security url or path provided"
-        clean 1
-    fi
+ # Prepare the package
+  directory_name=$(tar tf wazuh-dashboard.tar.gz | head -1 | sed 's#/.*##'  | sort -u)
+  tar -zxf wazuh-dashboard.tar.gz
+  rm wazuh-dashboard.tar.gz
+  mv $directory_name wazuh-dashboard-base
+  cp files/* wazuh-dashboard-base
+  echo ${version} > wazuh-dashboard-base/VERSION
+  tar -czf ./wazuh-dashboard.tar.gz wazuh-dashboard-base
 
-    if [ "${repository}" ];then
-        url="${repository}"
-    fi
-    # Copy the necessary files
-    cp ${current_path}/builder.sh ${dockerfile_path}
 
-    if [ "${build_base}" == "yes" ];then
-        # Base generation
-        if [ "${url}" ];then
-            base_cmd+="--app-url ${url} "
-        fi
-        if [ "${dashboard_url}" ];then
-            base_cmd+="--dashboard-url ${dashboard_url} "
-        fi
-        if [ "${dashboard_path}" ];then
-            base_cmd+="--dashboard-path ${dashboard_path} "
-        fi
-        if [ "${security_url}" ];then
-            base_cmd+="--security-url ${security_url} "
-        fi
-        if [ "${security_path}" ];then
-            base_cmd+="--security-path ${security_path} "
-        fi
-        if [ "${version}" ];then
-            base_cmd+="--version ${version} "
-        fi
-        echo ${base_cmd}
-        ../base/generate_base.sh -s ${outdir} -r ${revision} ${base_cmd}
-    fi
+  # Copy the necessary files
+  cp ${current_path}/builder.sh ${dockerfile_path}
 
-    # Build the Docker image
-    if [[ ${build_docker} == "yes" ]]; then
-        docker build -t ${container_name} ${dockerfile_path} || return 1
-    fi
+  # Build the Docker image
+  if [[ ${build_docker} == "yes" ]]; then
+    docker build -t ${container_name} ${dockerfile_path} || return 1
+  fi
 
-    # Build the Debian package with a Docker container
-    local_file='file://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-    if [[ $url =~ $local_file ]];then
-      local_path=`echo $url | sed 's/file:\/\///'`
-      file_name=`basename $local_path`
-      volumes="-v ${outdir}/:/tmp:Z -v ${local_path}:/tmp/${file_name}"
-    else
-        volumes="-v ${outdir}/:/tmp:Z"
-    fi
-    docker run -t --rm ${volumes} \
-        -v ${current_path}/../..:/root:Z \
-        ${container_name} ${architecture}  \
-        ${revision} ${url} ${version} || return 1
+  # Build the Debian package with a Docker container
+  volumes="-v ${outdir}/:/tmp:Z -v ${current_path}/wazuh-dashboard.tar.gz:/opt/wazuh-dashboard.tar.gz"
+  docker run -t --rm ${volumes} \
+    -v ${current_path}/../..:/root:Z \
+    ${container_name} ${architecture}  \
+    ${revision} ${version} || return 1
 
-    echo "Package $(ls -Art ${outdir} | tail -n 1) added to ${outdir}."
+  echo "Package $(ls -Art ${outdir} | tail -n 1) added to ${outdir}."
 
-    return 0
+  return 0
 }
 
 build() {
-    build_name=""
-    file_path=""
-    if [ "${architecture}" = "x86_64" ] || [ "${architecture}" = "amd64" ]; then
-        architecture="amd64"
-        build_name="${deb_amd64_builder}"
-        file_path="${deb_builder_dockerfile}/${architecture}"
-    else
-        echo "Invalid architecture. Choose: amd64 (x86_64 is accepted too)"
-        return 1
-    fi
+    build_name="${deb_amd64_builder}"
+    file_path="${deb_builder_dockerfile}/${architecture}"
     build_deb ${build_name} ${file_path} || return 1
-
     return 0
 }
 
@@ -129,20 +91,13 @@ help() {
     echo
     echo "Usage: $0 [OPTIONS]"
     echo
-    echo "    --app-url <url>            [Optional] Set the repository from where the Wazuh plugin should be downloaded. By default, will be used pre-release."
-    echo "    --dashboard-url <url>      Set the repository from where the .tar.gz file containing Wazuh Dashboard should be downloaded. "
-    echo "    --dashboard-path <path>    Set the location of the .tar.gz file containing the Wazuh Dashboard."
-    echo "    --security-url <url>       Set the repository from where the .zip file containing the Security plugin should be downloaded."
-    echo "    --security-path <path>     Set the location of the .zip file containing the security plugin."
     echo "    -v, --version <rev>        Wazuh version"
-    echo "    -a, --architecture <arch>  [Optional] Target architecture of the package [amd64]."
-    echo "    -b, --build-base <yes/no>  [Optional] Build a new base or use a existing one. By default, yes."
+    echo "    -p, --package <path>       Set the location of the .tar.gz file containing the Wazuh Dashboard package."
     echo "    -r, --revision <rev>       [Optional] Package revision. By default: 1."
     echo "    -s, --store <path>         [Optional] Set the destination path of package. By default, an output folder will be created."
     echo "    --dont-build-docker        [Optional] Locally built docker image will be used instead of generating a new one."
     echo "    -h, --help                 Show this help."
     echo
-    echo "    At least one of the app and one of the security options must be provided"
     exit $1
 }
 
@@ -154,65 +109,17 @@ main() {
         "-h"|"--help")
             help 0
             ;;
-        "-a"|"--architecture")
+        "-p"|"--package")
             if [ -n "${2}" ]; then
-                architecture="${2}"
+                package="${2}"
                 shift 2
             else
                 help 1
             fi
             ;;
-        "--app-url")
-            if [ -n "$2" ]; then
-                repository="$2"
-                shift 2
-            else
-                help 1
-            fi
-            ;;
-        "--dashboard-url")
-            if [ -n "$2" ]; then
-                dashboard_url="$2"
-                shift 2
-            else
-                help 1
-            fi
-            ;;
-        "--security-url")
-            if [ -n "$2" ]; then
-                security_url="$2"
-                shift 2
-            else
-                help 1
-            fi
-            ;;
-        "--dashboard-path")
-            if [ -n "$2" ]; then
-                dashboard_path="$2"
-                shift 2
-            else
-                help 1
-            fi
-            ;;
-        "--security-path")
-            if [ -n "$2" ]; then
-                security_path="$2"
-                shift 2
-            else
-                help 1
-            fi
-            ;;
-            "-v"|"--version")
+        "-v"|"--version")
             if [ -n "${2}" ]; then
                 version="${2}"
-                shift 2
-            else
-                help 1
-            fi
-            ;;
-        "-b"|"--build-base")
-            if [ -n "${2}" ]; then
-                build_base="${2}"
                 shift 2
             else
                 help 1
@@ -242,6 +149,10 @@ main() {
             help 1
         esac
     done
+
+    if [ -z "$package" ] | [ -z "$version" ] ; then
+      help 1
+    fi
 
     build || clean 1
 
--- dev-tools/build-packages/deb/builder.sh
+++ dev-tools/build-packages/deb/builder.sh
@@ -13,35 +13,8 @@ set -e
 target="wazuh-dashboard"
 architecture=$1
 revision=$2
-repository=$3
-version=$4
+version=$3
 directory_base="/usr/share/wazuh-dashboard"
-
-if [ -z "${revision}" ]; then
-    revision="1"
-fi
-
-if [ "${repository}" ];then
-    valid_url='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-    if [[ $repository =~ $valid_url ]];then
-    local_file='file://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-      if [[ $repository =~ $local_file ]];then
-        local_path=`echo $repository | sed 's/file:\/\///'`
-        file_name=`basename $local_path`
-        url="file:///tmp/${file_name}"
-      else
-        url="${repository}"
-      fi
-        if ! curl --output /dev/null --silent --head --fail "${url}"; then
-            echo "The given URL to download the Wazuh plugin zip does not exist: ${url}"
-            exit 1
-        fi
-    else
-        url="https://packages-dev.wazuh.com/${repository}/ui/dashboard/wazuh-${version}-${revision}.zip"
-    fi
-else
-    url="https://packages-dev.wazuh.com/pre-release/ui/dashboard/wazuh-${version}-${revision}.zip"
-fi
 
 # Build directories
 build_dir=/build
@@ -58,8 +31,6 @@ cp -r /root/build-packages/deb/debian/* ${source_dir}/debian/
 cd ${build_dir}/${target} && tar -czf ${pkg_name}.orig.tar.gz "${pkg_name}"
 
 # Configure the package with the different parameters
-echo ${version}
-echo ${revision}
 sed -i "s:VERSION:${version}:g" ${source_dir}/debian/changelog
 sed -i "s:RELEASE:${revision}:g" ${source_dir}/debian/changelog
 sed -i "s:export INSTALLATION_DIR=.*:export INSTALLATION_DIR=${directory_base}:g" ${source_dir}/debian/rules
