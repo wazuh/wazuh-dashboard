2a94512aa07acec1bb90803b344ee866ecedc1fe
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2024-02-20 21:07:36 +0100

    140 update the repo with the latest changes of wazuh dashboard (#141)

--- src/core/public/chrome/chrome_service.tsx
+++ src/core/public/chrome/chrome_service.tsx
@@ -269,7 +269,7 @@ export class ChromeService {
           forceAppSwitcherNavigation$={navLinks.getForceAppSwitcherNavigation$()}
           helpExtension$={helpExtension$.pipe(takeUntil(this.stop$))}
           helpSupportUrl$={helpSupportUrl$.pipe(takeUntil(this.stop$))}
-          homeHref={application.getUrlForApp('home')}
+          homeHref={application.getUrlForApp('/app/wz-home')}
           isVisible$={this.isVisible$}
           opensearchDashboardsVersion={injectedMetadata.getWazuhVersion()}
           navLinks$={navLinks.getNavLinks$()}
