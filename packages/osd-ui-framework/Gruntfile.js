f007b57e23bcd862ee1da4d4eac9a12221b48849
Guido Modarelli <guidomodarelli25@gmail.com>
2024-12-24 12:45:29 -0300

    Add Wazuh themed CSS and SCSS files to the UI framework for both light and dark modes

--- packages/osd-ui-framework/Gruntfile.js
+++ packages/osd-ui-framework/Gruntfile.js
@@ -99,6 +99,8 @@ module.exports = function (grunt) {
       uiFrameworkCompile('src/kui_next_dark.scss', 'dist/kui_next_dark.css'),
       uiFrameworkCompile('src/kui_v9_light.scss', 'dist/kui_v9_light.css'),
       uiFrameworkCompile('src/kui_v9_dark.scss', 'dist/kui_v9_dark.css'),
+      uiFrameworkCompile('src/kui_wazuh_light.scss', 'dist/kui_wazuh_light.css'),
+      uiFrameworkCompile('src/kui_wazuh_dark.scss', 'dist/kui_wazuh_dark.css'),
     ]).then(done);
   });
 };
