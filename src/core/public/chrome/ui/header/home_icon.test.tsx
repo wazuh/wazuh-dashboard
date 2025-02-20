345575a7b3cf6b62839514d652b8cb6b10783f96
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2025-02-20 13:54:19 +0100

    Update src/core/public/chrome/ui/header/home_icon.test.tsx
    Co-authored-by: Antonio <34042064+Desvelao@users.noreply.github.com>
    Signed-off-by: Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>

--- src/core/public/chrome/ui/header/home_icon.test.tsx
+++ src/core/public/chrome/ui/header/home_icon.test.tsx
@@ -61,7 +61,7 @@ describe('Home icon,', () => {
       expect(icon.prop('data-test-subj')).toEqual('defaultMark');
       expect(icon.prop('type')).toEqual(props.logos.Mark.url);
       expect(icon.prop('size')).toEqual('l');
-      expect(icon.prop('title')).toEqual('Wazuh dashboards home');
+      expect(icon.prop('title')).toEqual('Wazuh dashboard home');
 
       expect(component).toMatchSnapshot();
     });
