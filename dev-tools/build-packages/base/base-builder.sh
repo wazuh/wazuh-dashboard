88780283c2ef4a0c0f68c18f90a487479ebc66f2
Guido Modarelli <guidomodarelli25@gmail.com>
2025-08-25 14:50:52 +0000

    improve assistant dashboard file handling and compression
    Refactors variable names for clarity and consistency in assistant dashboard logic. Updates image replacement and text substitution processes. Enhances file compression settings with higher gzip and brotli compression levels for better optimization.

--- dev-tools/build-packages/base/base-builder.sh
+++ dev-tools/build-packages/base/base-builder.sh
@@ -42,27 +42,19 @@ js-file() {
 }
 
 assistant_dashboard_whitelabeling() {
-  PLUGIN_NAME="assistantDashboards"
-	CHUNK_FILE=$(js-file $PLUGIN_NAME "chunk.10")
-	PLUGIN_FILE=$(js-file $PLUGIN_NAME "plugin")
+  local plugin_name="assistantDashboards"
+	local ASSISTANT_DASHBOARD_CHUNK_FILE=$(js-file $plugin_name "chunk.10")
+	local ASSISTANT_DASHBOARD_PLUGIN_FILE=$(js-file $plugin_name "plugin")
 
 	OLD_IMAGE="base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHZpZXdCb3g9IjAgMCA0MCA0MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNMzAuODMzMyAzOS44NTlDMzEuODk2MiAzOS40Mzk4IDMxLjg2OCAzOC4xMTU5IDMxLjg2OCAzNy4wMjk0VjMyLjY2NjZIMzQuMjg1N0MzNy40NDE3IDMyLjY2NjYgNDAgMzAuMjUzOSA0MCAyNy4yNzc1VjcuMzg5MTRDND.*InVzZXJTcGFjZU9uVXNlIj4KICAgIDxzdG9wIHN0b3AtY29sb3I9IiMwMEEzRTAiLz4KICAgIDxzdG9wIG9mZnNldD0iMSIgc3RvcC1jb2xvcj0iIzAwQTNFMCIgc3RvcC1vcGFjaXR5PSIwIi8+CiAgICA8L2xpbmVhckdyYWRpZW50PgogICAgPC9kZWZzPgo8L3N2Zz4="
 
 	NEW_IMAGE="base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCIKCSB2aWV3Qm94PSIwIDAgNTEyIDUxMiIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgNTEyIDUxMjsiIHhtbDpzcGFjZT0icHJlc2VydmUiPgo8Zz4KCTxwYXRoIGZpbGw9IiMwMDZCQjQiIGQ9Ik00MDQuNSwyMS4yVjBoLTI5N0M0OC45LDAsMS40LDQ3LjUsMS40LDEwNi4xdjE2OS43YzAsNTguNiw0Ny41LDEwNi4xLDEwNi4xLDEwNi4xaDI0Ni45TDUxMC42LDUxMlYxMDYuMQoJCUM1MTAuNiw0Ny41LDQ2My4xLDAsNDA0LjUsMFYyMS4ydjIxLjJjMzUuMSwwLjEsNjMuNiwyOC41LDYzLjYsNjMuNnYzMTUuM2wtOTguNC04MkgxMDcuNWMtMzUuMS0wLjEtNjMuNi0yOC41LTYzLjYtNjMuNmwwLTE2OS43CgkJYzAuMS0zNS4xLDI4LjUtNjMuNiw2My42LTYzLjZoMjk3VjIxLjJ6IE00MDQuNSwxMjcuM0gxMjguN3Y0Mi40aDI3NS44VjEyNy4zeiBNNDA0LjUsMjEyLjFIMjEzLjZ2NDIuNGgxOTAuOVYyMTIuMXoiLz4KPC9nPgo8L3N2Zz4K"
 
+	sed -i -e "s|OpenSearch Assistant|Dashboard assistant|g" $ASSISTANT_DASHBOARD_CHUNK_FILE
+	sed -i -e "s|OpenSearch Assistant|Dashboard assistant|g" $ASSISTANT_DASHBOARD_PLUGIN_FILE
+	sed -i -e "s|he Dashboard assistant|he dashboard assistant|g" $ASSISTANT_DASHBOARD_PLUGIN_FILE
 
-
-	sed -i -e "s|OpenSearch Assistant|Dashboard assistant|g" $CHUNK_FILE
-	sed -i -e "s|OpenSearch Assistant|Dashboard assistant|g" $PLUGIN_FILE
-	sed -i -e "s|he Dashboard assistant|he dashboard assistant|g" $PLUGIN_FILE
-
-	sed -i -e "s|$OLD_IMAGE|$NEW_IMAGE|" $PLUGIN_FILE
-
-	gzip -c -9 "$PLUGIN_FILE" > "$PLUGIN_FILE.gz"
-	gzip -c -9 "$CHUNK_FILE" > "$CHUNK_FILE.gz"
-
-	brotli -q 11 -f "$PLUGIN_FILE" -o "$PLUGIN_FILE.br"
-	brotli -q 11 -f "$CHUNK_FILE" -o "$CHUNK_FILE.br"
+	sed -i -e "s|$OLD_IMAGE|$NEW_IMAGE|" $ASSISTANT_DASHBOARD_PLUGIN_FILE
 }
 
 # Paths
@@ -153,6 +145,8 @@ log
 
 # Generate compressed files
 files_to_recreate=(
+  $(js-file $plugin_name "chunk.10")
+  $(js-file $plugin_name "plugin")
   $(js-file "reportsDashboards" "plugin")
   $(js-file "alertingDashboards" "plugin")
   $(js-file "customImportMapDashboards" "plugin")
@@ -161,8 +155,8 @@ files_to_recreate=(
 )
 
 for value in "${files_to_recreate[@]}"; do
-  gzip -c "$value" >"$value.gz"
-  brotli -c "$value" >"$value.br"
+  gzip -c -9 "$value" >"$value.gz"
+  brotli -c -q 11 -f "$value" >"$value.br"
 done
 
 log
