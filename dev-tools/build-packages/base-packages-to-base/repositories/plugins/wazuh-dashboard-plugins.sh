7fe2487a4f93e350880451e934bf0935533e7766
Antonio <34042064+Desvelao@users.noreply.github.com>
2025-11-10 18:18:03 +0100

    Update indexer resource on wazuh plugin install (#995)
    * feat: move git ref definition to plugin dependencies installation
    
    * fix: add GIT_REF env var to the step for bootstraping core with plugin on build workflow

--- dev-tools/build-packages/base-packages-to-base/repositories/plugins/wazuh-dashboard-plugins.sh
+++ dev-tools/build-packages/base-packages-to-base/repositories/plugins/wazuh-dashboard-plugins.sh
@@ -6,9 +6,9 @@ mv wazuh-dashboard-plugins/plugins/* ./
 mkdir /home/node/packages/wazuh-dashboard-plugins
 for wazuh_dashboard_plugin in $wazuh_dashboard_plugins; do
   cd $base_path_plugins/$wazuh_dashboard_plugin
-  yarn install
+  GIT_REF="${WAZUH_DASHBOARD_PLUGINS_BRANCH}" yarn install
   echo "Building $wazuh_dashboard_plugin"
-  GIT_REF="${WAZUH_DASHBOARD_PLUGINS_BRANCH}" yarn build
+  yarn build
   echo "Copying $wazuh_dashboard_plugin"
   package_name=$(jq -r '.id' ./opensearch_dashboards.json)
   cp $base_path_plugins/$wazuh_dashboard_plugin/build/$package_name-$OPENSEARCH_DASHBOARDS_VERSION.zip /home/node/packages/wazuh-dashboard-plugins/$package_name-$OPENSEARCH_DASHBOARDS_VERSION.zip
