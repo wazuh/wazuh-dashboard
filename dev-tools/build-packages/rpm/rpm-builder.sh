6a088824c0130e827170d8ae5f1b04794bbac88f
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2026-06-09 13:24:26 -0300

    Refactor production mode determination in build script

--- dev-tools/build-packages/rpm/rpm-builder.sh
+++ dev-tools/build-packages/rpm/rpm-builder.sh
@@ -58,7 +58,9 @@ log "Extracting base tar.gz..."
 tar -zxf "${out_dir}/wazuh-dashboard-$version-$revision-linux-$architecture.tar.gz"
 log "Preparing the package..."
 jq '.wazuh.revision="'${revision}'"' package.json > pkgtmp.json && mv pkgtmp.json package.json
-if [ "${is_production}" = "yes" ]; then
+# TODO: Review if we want to set production mode based on is_stage or if we want to have a separate input for that.
+# if [ "${is_production}" = "yes" ]; then
+if false; then
   jq '.wazuh.isProduction = true' package.json > pkgtmp.json && mv pkgtmp.json package.json
 else
   jq '.wazuh.isProduction = false' package.json > pkgtmp.json && mv pkgtmp.json package.json
