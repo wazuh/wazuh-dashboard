2aedc23d870b8f3260c5c724f6dc490abaac8296
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2026-06-09 14:14:50 -0300

    Revert production mode condition in deb package build script

--- dev-tools/build-packages/deb/deb-builder.sh
+++ dev-tools/build-packages/deb/deb-builder.sh
@@ -61,8 +61,7 @@ tar -zxf "${out_dir}/wazuh-dashboard-$version-$revision-linux-$architecture.tar.
 log "Preparing the package..."
 jq '.wazuh.revision="'${revision}'"' package.json > pkgtmp.json && mv pkgtmp.json package.json
 # TODO: Review if we want to set production mode based on is_stage or if we want to have a separate input for that.
-# if [ "${is_production}" = "yes" ]; then
-if false; then
+if [ "${is_production}" = "yes" ]; then
   jq '.wazuh.isProduction = true' package.json > pkgtmp.json && mv pkgtmp.json package.json
 else
   jq '.wazuh.isProduction = false' package.json > pkgtmp.json && mv pkgtmp.json package.json
