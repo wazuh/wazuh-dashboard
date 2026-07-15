c1ec7fd00856aab00fc1153c184a8bbc458ad1a7
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2026-07-15 13:08:49 +0200

    feat(discover): add reference to max row in csv export setting

--- src/plugins/discover/public/application/components/download_csv/download_csv_callout.tsx
+++ src/plugins/discover/public/application/components/download_csv/download_csv_callout.tsx
@@ -5,9 +5,10 @@
 
 import './download_csv_callout.scss';
 import React from 'react';
-import { EuiCallOut, EuiText } from '@elastic/eui';
+import { EuiCallOut, EuiLink, EuiText } from '@elastic/eui';
 import { FormattedMessage } from '@osd/i18n/react';
-import { MAX_DOWNLOAD_CSV_COUNT } from './constants';
+import { MAX_DOWNLOAD_CSV_COUNT, REPORTS_CSV_MAX_ROWS_SETTING } from './constants';
+import { getServices } from '../../../opensearch_dashboards_services';
 
 export interface DiscoverDownloadCsvCalloutProps {
   max?: number;
@@ -16,6 +17,8 @@ export interface DiscoverDownloadCsvCalloutProps {
 export const DiscoverDownloadCsvCallout = ({
   max = MAX_DOWNLOAD_CSV_COUNT,
 }: DiscoverDownloadCsvCalloutProps) => {
+  const { addBasePath } = getServices();
+
   return (
     <EuiCallOut
       className="dscDownloadCsvCallout"
@@ -25,8 +28,26 @@ export const DiscoverDownloadCsvCallout = ({
       <EuiText size="s" className="dscDownloadCsvCallout__text">
         <FormattedMessage
           id="discover.downloadCsvCallout"
-          defaultMessage="There is a limit of {max} total result downloads."
-          values={{ max: max.toLocaleString() }}
+          defaultMessage="There is a limit of {max} total result downloads.{lineBreak}This value can be changed using the {settingName} setting in {advancedSettingsLink}."
+          values={{
+            max: max.toLocaleString(),
+            lineBreak: <br />,
+            settingName: <strong>{REPORTS_CSV_MAX_ROWS_SETTING}</strong>,
+            advancedSettingsLink: (
+              <EuiLink
+                target="_blank"
+                data-test-subj="dscDownloadCsvCalloutAdvancedSettingsLink"
+                href={addBasePath(
+                  `/app/management/opensearch-dashboards/settings/${REPORTS_CSV_MAX_ROWS_SETTING}`
+                )}
+              >
+                <FormattedMessage
+                  id="discover.downloadCsvCallout.advancedSettingsLinkText"
+                  defaultMessage="Dashboard Management > Advanced settings"
+                />
+              </EuiLink>
+            ),
+          }}
         />
       </EuiText>
     </EuiCallOut>
